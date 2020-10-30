c2e9b9853a7bf3c4ec300cf44fab8f730967b2ba
Francisco <francisjur@unicauca.edu.co>
2020-10-30 15:32:39 -0500

    items Menu, Logo Favoritos, ScannerQR

--- app/src/main/java/edu/unicauca/practice_layout/MainActivity.java
+++ app/src/main/java/edu/unicauca/practice_layout/MainActivity.java
@@ -2,13 +2,30 @@ package edu.unicauca.practice_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonInicio = (Button)findViewById(R.id.buttonLogin);
+
+        buttonInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/edu/unicauca/practice_layout/MenuActivity.java
+++ app/src/main/java/edu/unicauca/practice_layout/MenuActivity.java
@@ -1,5 +1,34 @@
 package edu.unicauca.practice_layout;
 
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MenuActivity {
+public class MenuActivity extends AppCompatActivity{
+
+    private ListView listMenu;
+    private ImageButton scannerQr;
+    private ImageButton fav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        String[] list = getResources().getStringArray(R.array.array_menu);
+        listMenu = (ListView)findViewById(R.id.listM);
+        scannerQr = (ImageButton)findViewById(R.id.bt_qr);
+        fav=(ImageButton)findViewById(R.id.bt_fav);
+
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+        listMenu.setAdapter(arrayAdapter);
+
+    }
+
 }
