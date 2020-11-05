a00c195b73eba2114de4dbf5a4f4f6cffa81845f
Francisco <francisjur@unicauca.edu.co>
2020-11-04 23:43:46 -0500

    Funcionalidad inicial LogIn Google con firebase

--- app/src/main/java/edu/unicauca/practice_layout/InicioSActivity.java
+++ app/src/main/java/edu/unicauca/practice_layout/InicioSActivity.java
@@ -3,15 +3,90 @@ package edu.unicauca.practice_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InicioSActivity extends AppCompatActivity {
 
-    private Button buttonInicio;
+    FirebaseAuth mfirebaseAutH;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    public static final int REQUEST_CODE =  9999;
+    List<AuthUI.IdpConfig> provider = Arrays.asList(
+            new AuthUI.IdpConfig.GoogleBuilder().build()
+    );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_s);
+        mfirebaseAutH = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
+                if(user !=null){
+                    Toast.makeText(InicioSActivity.this, "Iniciaste Sesión Ok", Toast.LENGTH_SHORT).show();
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }else{
+                    startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(provider)
+                            .setIsSmartLockEnabled(false)
+                            .build(), REQUEST_CODE
+                    );
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mfirebaseAutH.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mfirebaseAutH.removeAuthStateListener(mAuthListener);
+    }
+
+    /*public void cerrarsesion (View view){
+        AuthUI.getInstance().signOut((this)).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(InicioSActivity.this, "Sesion cerrada", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }*/
+
+    /*private Button buttonInicio;
     private TextView labelRegistro;
 
     @Override
@@ -37,5 +112,5 @@ public class InicioSActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-    }
+    }*/
 }
